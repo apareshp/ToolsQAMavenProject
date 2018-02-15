5925546272098dc51e29a8829ee17ba418320acc
apareshp <apareshp@gmail.com>
2018-02-15 17:56:42 +0530

    first commit

---
+++ src/test/java/businessComponents/NewTest.java
@@ -0,0 +1,16 @@
+package businessComponents;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  WebDriver driver;
+	  System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/BrowserDriversEXE/chromedriver.exe");
+	  driver = new ChromeDriver();
+	  driver.get("http://toolsqa.com/");
+	  driver.manage().window().maximize();
+  }
+}
